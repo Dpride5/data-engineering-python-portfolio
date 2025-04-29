ac408bac7dccad3a628bc7c7836d385b5e1fa7b6
Darion Pride <Dpride5@wgu.edu>
2025-04-29 13:58:54 -0500

    Attempting another drill to solidify my understanding

---
+++ Core/01_basics_drill(1).py
@@ -0,0 +1,8 @@
+# Training the basic step I took earlier this morning
+# Making sure the information sticks (done without a reference, purely off memory)
+poppy = [1,5,10,15]
+
+def double(x):
+    return x * 5
+
+print([double(poppys) for poppys in poppy])
